44d4ab7049ec2b7bddca5adbd9ccaad2a57e83ec
VictorVaquerosoto <54574705+VictorVaquerosoto@users.noreply.github.com>
2019-11-05 17:07:21 +0100

    Add files via upload

--- ui.R
+++ ui.R
@@ -8,9 +8,13 @@ suppressWarnings(suppressMessages(library(rattle)))
 suppressWarnings(suppressMessages(library(rpart)))
 suppressWarnings(suppressMessages(library(plotly)))
 shinyUI(fluidPage(
-  titlePanel("Predict the fower class from the length and width of the petal using a decision tree model"),
+  titlePanel("Predict the flower class from the length and width of the petal using a decision tree model"),
   sidebarLayout(
     sidebarPanel(
+      ("This application works with the iris dataset. 
+      This data set contains 3 classes of 50 instances each, where each class refers to a type of iris plant.
+       The idea of this app is to classify the type of flower based on the length and width of its petal.
+       For that, please, select a Length and width petal and see the result."),
       sliderInput("sliderPetalLength", "What is the length of the petal?", 0, 10, value = 0,step=0.1),
       sliderInput("sliderPetalWidth", "What is the width of the petal?", 0, 4, value = 0,step=0.1),
       submitButton("Submit")
